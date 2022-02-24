8c8fd4f1cd92637fc395ac268e09557af9b52839
+ <+>
2022-02-23 20:38:01 -0500

    commit tree 8ef84b1853aec6ad6a56913dd896c15a81564fff

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+// Code your solution in this file!\
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const returnFirstTwoDrivers = function(){return ['Antonia', 'Nuru']};
+const returnLastTwoDrivers = function(){return ['Amari', 'Mo']};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(multiplier){
+    return function(fare){
+        return fare * multiplier;
+    }
+}
+const fareDoubler = function(fare){
+    return fare * 2;
+}
+const fareTripler = function(fare){
+    return fare * 3;
+}
+const selectDifferentDrivers = function(drivers , randomFunction){
+    if (randomFunction === returnFirstTwoDrivers){
+    return ['Antonia','Nuru']}
+    else {
+        return ['Amari','Mo']
+    }
+    }
